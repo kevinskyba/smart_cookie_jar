e9ceec6fc869e2bdb502e48e711aea1afcbe8420
Kevin Skyba <kevinskyba@live.de>
2019-01-07 20:26:18 +0100

    removed old nodemcu code added fritzing file

--- nodemcu/README.md
+++
@@ -1,3 +0,0 @@
-# Smart Cookie Jar Hardware
-
-TODO
--- nodemcu/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "config.h"
-#include "Arduino.h"
-
-#ifdef MODE_CONFIGURATION
-#include "calibration.h"
-#else
-#include "smart_jar.h"
-#endif
